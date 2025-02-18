26a2ff45af4e0c841d5905733a681947a301f95d
Turghyaen Rajanantran <turghyaenr@gmail.com>
2025-02-19 07:24:09 +0800

    path update

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json()); 
 
 // MongoDB Connection
-mongoose.connect(config.mongoURI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
